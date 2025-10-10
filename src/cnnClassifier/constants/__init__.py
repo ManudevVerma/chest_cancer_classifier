eb2ff4e5cd6920f4b77a140d9915d7ca7756ae2d
Manudev Verma <manudevverma429@gmail.com>
2025-10-10 22:59:25 +0530

    update committed successfully on 10 10 2025

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,5 @@
+#These file paths are constant and do not change unless we change them manually
+#So we are reading them  here
+from pathlib import Path
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
